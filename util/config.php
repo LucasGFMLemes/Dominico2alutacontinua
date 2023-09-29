555a84351bfe2886f1598d11a0e516594661c02f
Laboratório <lucasgfm2005@gmail.com>
2023-09-29 15:02:04 -0300

    first commit

---
+++ util/config.php
@@ -0,0 +1,10 @@
+<?php
+
+//Configurar essas variáveis de acordo com o seu ambiente
+define("DB_HOST", "localhost");
+define("DB_NAME", "crud_aula");
+define("DB_USER", "root");
+define("DB_PASSWORD", "bancodedados");
+
+//Constante com a URL do sistema
+define("BASE_URL", "/aula.ddomenico/crud_alunos");
